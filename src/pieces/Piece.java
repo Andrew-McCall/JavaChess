6cebd837c856866c9405690396e52053aff4395b
Andrew McCall <amccall@qa.com>
2022-04-01 21:49:26 +0100

    Created Piece Interface and Enums

---
+++ src/pieces/Piece.java
@@ -0,0 +1,28 @@
+package pieces;
+
+interface Piece {
+
+	public Name getName();
+	public Side getSide();
+	public int[][] getMoves();
+	
+	public int getX();
+	public int getY();
+	public void setX();
+	public void setY();
+	
+}
+
+enum Name {
+	KING,
+	QUEEN,
+	ROOK,
+	KNIGHT,
+	BISHOP,
+	PAWN,
+}
+
+enum Side{
+	BLACK, 
+	WHITE,
+}

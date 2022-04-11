cbfe386a0ccd74f5ea7004b2153feb42abba0cec
Andrew McCall <amccall@qa.com>
2022-04-11 15:21:15 +0100

    Added Public Domain Chess Icons

--- src/Board.java
+++ src/Board.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 

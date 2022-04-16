d0615c20dbfec90cf61fd13e229a0de1d0730733
Andrew McCall <amccall@qa.com>
2022-04-16 13:43:15 +0100

    Rook Logic Completed

--- src/pieces/Rook.java
+++ src/pieces/Rook.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import Main.Main;
 import enums.Name;
 import enums.Side;
 
@@ -52,6 +53,58 @@ public class Rook extends Piece{
 	public Name getName() {
 		return Name.ROOK;
 	}
+	
+	@Override
+	public boolean MoveLegal(int x, int y) {
 
+		Piece target = Main.getGameLogic().getPiece(x,y);
+		
+		boolean xSame = (x == getX());
+		boolean ySame = (y == getY());
+
+		if ((target != null && target.getSide() == getSide())) {
+			return false;
+		} else if (ySame) {
+			if (x>getX()) {
+				x--;
+			}else {
+				x++;
+			}
+			while (x != getX()) {
+				
+				if (Main.getGameLogic().getPiece(x,y) != null) return false;
+				
+				if (x>getX()) {
+					x--;
+				}else {
+					x++;
+				}
+			}
+			
+			return true;
+			
+		} else if (xSame) {
+			if (y>getY()) {
+				y--;
+			}else {
+				y++;
+			}
+			while (y != getY()) {
+				
+				if (Main.getGameLogic().getPiece(x,y) != null) return false;
+				
+				if (y>getY()) {
+					y--;
+				}else {
+					y++;
+				}
+			}
+
+			return true;
+			
+		}
+		
+		return false;
+	}
 
 }
